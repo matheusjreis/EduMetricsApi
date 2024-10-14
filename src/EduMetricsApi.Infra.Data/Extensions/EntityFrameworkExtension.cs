e95a92260fc7bcffe118cf22be46844801a6d670
Matheus Reis <matheus.reis@asaconsultoria.com.br>
2024-10-14 20:57:21 -0300

    [REFACTOR] Removing useless method and useless classes.

--- src/EduMetricsApi.Infra.Data/Extensions/EntityFrameworkExtension.cs
+++ src/EduMetricsApi.Infra.Data/Extensions/EntityFrameworkExtension.cs
@@ -8,53 +8,6 @@ public static class EntityFrameworkExtension
 {
     private static int _depth { get; set; } = 0;
 
-    public static IQueryable<TEntity> IncludeRecursively<TEntity>(this IQueryable<TEntity> queryable, ICollection<string>? exclude = null) where TEntity : class
-    {
-        var entityType = typeof(TEntity);
-        var properties = entityType.GetProperties().Where(w => exclude == null ? w == w : !exclude.Contains(w.Name));
-
-        foreach (var property in properties)
-        {
-            var propertyType = property.PropertyType;
-
-            if (propertyType.IsClass && propertyType != typeof(string) && (exclude is null || (exclude is not null && !exclude.Contains(property.Name))))
-            {
-                var p = new List<string>();
-                queryable = queryable.Include(property.Name);
-
-                _depth = 0;
-                GetPropertiesRecursive(Type.GetType($"{property.PropertyType.FullName}, {property.PropertyType.Assembly.FullName}")!, p);
-
-                foreach (var item in p)
-                {
-                    queryable.Include(property.Name + "." + item);
-                }
-            }
-
-            // SE FOR UMA LISTA
-            if (propertyType.IsAbstract)
-            {
-                var listProp = propertyType.GetGenericArguments()[0];
-                foreach (var props in listProp.GetProperties())
-                {
-                    if (props.PropertyType.IsClass && props.PropertyType != typeof(string))
-                    {
-                        if (exclude is not null && exclude.Contains(property.Name))
-                        {
-                            continue;
-                        }
-
-                        queryable = queryable.Include($"{listProp.Name}.{props.Name}");
-                    }
-                }
-
-                queryable = queryable.Include(listProp.Name);
-            }
-        }
-
-        return queryable;
-    }
-
     private static void GetPropertiesRecursive(Type type, List<string> propertyNames, string prefix = "", int depth = 10)
     {
         var properties = type.GetProperties();
